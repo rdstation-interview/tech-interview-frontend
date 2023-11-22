a2fc21f5695cd677f2003f236f097b57fb36929c
João Paulo Barbosa <jpaulobneto@gmail.com>
2023-11-21 23:38:35 -0300

    chore: adjusting the quest

--- packages/main/api/workflows/validate-name.js
+++ packages/main/api/workflows/validate-name.js
@@ -1,35 +1,15 @@
-import { workflows } from '../workflows'
+/**
+ * Fluxos existentes podem ser importados da seguinte maneira
+ * import { workflows } from '../workflows'
+ */
 
-const isValidName = (queryName = '') => {
-  if (queryName === '')
-    return {
-      valid: false,
-      reason: 'O nome do fluxo não pode ser deixado em branco.',
-    }
-
-  const index = workflows.findIndex(
-    ({ name }) => formatName(name) === formatName(queryName),
-  )
-
-  const valid = index === -1
-
-  return {
-    valid,
-    reason: !valid
-      ? 'O nome que você escolheu já está sendo utilizado.'
-      : undefined,
-  }
-}
-
-const formatName = name => {
-  return name.trim().toLowerCase()
-}
-
+/**
+ * Em caso de uma requisição GET para esse endpoint, a função abaixo será executada.
+ * @param {Object} req - A request. Use a propriedade "query" para ler as query strings da URL.
+ * @param {Object} res - A response. Use a função "json" para formatar sua resposta.
+ */
 export const get = (req, res) => {
-  const { name: queryName } = req.query
-
   res.json({
-    value: queryName,
-    ...isValidName(queryName),
+    status: 200,
   })
 }
--- packages/main/src/Editor/Editor.js
+++ packages/main/src/Editor/Editor.js
@@ -1,16 +1,14 @@
 import {
   Button,
   ButtonGroup,
-  Form,
-  Input,
-  Modal,
   Navbar,
   Text,
 } from '@resultadosdigitais/tangram-components'
 import { Pen } from '@resultadosdigitais/tangram-react-icons'
-import { useRef, useState } from 'react'
 import styled from 'styled-components'
-import { validateWorkflowName } from '../apiClient'
+
+// Você pode chamar o endpoint de validação coma a função a seguir
+// import { validateWorkflowName } from '../apiClient'
 
 const Root = styled.div`
   background: var(--neutral-surface-high-emphasis);
@@ -18,27 +16,6 @@ const Root = styled.div`
 `
 
 function App() {
-  const nameRef = useRef(null)
-  const [workflowName, setWorkflowName] = useState('Fluxo sem nome')
-  const [nameModalOpen, setNameModalOpen] = useState(false)
-  const [nameValidation, setNameValidation] = useState({ valid: true })
-
-  const openNameModal = () => setNameModalOpen(true)
-  const closeNameModal = () => setNameModalOpen(false)
-
-  const onChangeName = async () => {
-    const { value: name } = nameRef.current
-
-    const validation = await validateWorkflowName(name)
-
-    setNameValidation(validation)
-
-    if (validation.valid) {
-      closeNameModal()
-      setWorkflowName(name)
-    }
-  }
-
   return (
     <Root>
       <Navbar>
@@ -50,9 +27,8 @@ function App() {
           <Button
             kind={Button.kinds.tertiary}
             endIcon={<Pen title="Renomear fluxo" />}
-            onClick={openNameModal}
           >
-            {workflowName}
+            Fluxo sem nome
           </Button>
         </Navbar.Section>
 
@@ -63,46 +39,6 @@ function App() {
           </ButtonGroup>
         </Navbar.Section>
       </Navbar>
-      <Text token={Text.tokens.TEXT_XL_BOLD}>Editor</Text>
-
-      <Modal
-        open={nameModalOpen}
-        onClose={closeNameModal}
-        aria-describedby="modalDescription"
-        aria-labelledby="modalTitle"
-      >
-        <Modal.Header id="modalTitle">Editar nome do fluxo</Modal.Header>
-
-        <Modal.Content>
-          <Text>
-            Utilize um nome de fácil memorização. Ele será utilizado para sua
-            identificação dentro do RD Station.
-          </Text>
-          <Form.Control error={!nameValidation.valid}>
-            <Form.Label htmlFor="inputField">Nome do fluxo</Form.Label>
-            <Input
-              ref={nameRef}
-              id="inputField"
-              name="inputField"
-              autoComplete="off"
-            />
-            {!nameValidation.valid && (
-              <Form.Feedback id="nameFeedback">
-                {nameValidation.reason}
-              </Form.Feedback>
-            )}
-          </Form.Control>
-        </Modal.Content>
-
-        <Modal.Actions>
-          <ButtonGroup>
-            <Button kind={Button.kinds.secondary} onClick={closeNameModal}>
-              Cancelar
-            </Button>
-            <Button onClick={onChangeName}>Confirmar</Button>
-          </ButtonGroup>
-        </Modal.Actions>
-      </Modal>
     </Root>
   )
 }
