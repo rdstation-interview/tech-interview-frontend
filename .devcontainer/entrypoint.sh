4729259b813ce2aa6272b72e26c80f1387bcb72c
João Paulo Barbosa <joao.barbosa@rdstation.com>
2024-07-03 10:54:16 -0300

    chore: fix yarn install

--- .devcontainer/entrypoint.sh
+++ .devcontainer/entrypoint.sh
@@ -6,4 +6,7 @@ echo "    npmRegistryServer: ${JFROG_NPM_REPOSITORY}" >>~/.yarnrc.yml
 echo "    npmAlwaysAuth: true" >>~/.yarnrc.yml
 echo "    npmAuthIdent: ${JFROG_USERNAME}:${JFROG_TOKEN}" >>~/.yarnrc.yml
 
+echo "> Executing 'yarn install' on web"
+yarn install
+
 exec bash
