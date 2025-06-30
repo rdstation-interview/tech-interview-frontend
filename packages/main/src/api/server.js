333b4ec988697a6bb5acc04bceb5ac5013fa64ba
João Paulo Barbosa <joao.barbosa@rdstation.com>
2025-06-30 04:09:58 +0000

    chore: create server

---
+++ packages/main/src/api/server.js
@@ -0,0 +1,22 @@
+// src/mirage.js
+import { createServer, Model } from 'miragejs'
+
+export function startMirage({ environment = 'development' } = {}) {
+  return createServer({
+    environment,
+    routes() {
+      this.namespace = 'api'
+
+      this.get('/workflows/validate-name', (_, request) => {
+        const { name } = request.queryParams
+        const workflows = [
+          { id: 'WF01', name: 'Dia das crianças' },
+          { id: 'WF02', name: 'Dia das mães' },
+          { id: 'WF03', name: 'Dia dos pais' },
+        ]
+
+        return null
+      })
+    },
+  })
+}
